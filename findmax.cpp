09e9412dfea2a3931473b3d3b06a3fb9f08f9065
tsmirnovv <timsm976@gmail.com>
2022-12-18 20:42:21 +0300

    code added

---
+++ findmax.cpp
@@ -0,0 +1,16 @@
+﻿#include <iostream>
+using namespace std;
+
+int main()
+{
+    int nums[4] = { 1,2,4,3 };
+    int max = 0;
+    for (int i = 0; i < 4; i++) {
+        if (nums[i] >  max) {
+            max == nums[i];
+        }
+    }
+    cout << max;
+}
+
+
